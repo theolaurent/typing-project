423267b682383a625cfc4ab83944f26f8598f2d2
Theo Laurent <theo.laurent@ens.fr>
2017-02-17 12:39:14 +0100

    Defunct: WIP (3).

--- src/defunct.ml
+++ src/defunct.ml
@@ -1,13 +1,14 @@
 open Atom
 open Types
 open Terms
+open Symbols
 
-(* TODO: comment code *)
-
+(* the new arrow type constructor *)
+let arrow_tk : atom = fresh (Identifier.mk "arrow" (4, "type constructor"))
 let arrow_type (a1 : ftype) (a2 : ftype) =
-  let arrow_tk : atom = fresh (Identifier.mk "arrow" (4, "type constructor")) in
   TyCon (arrow_tk, [a1 ; a2])
 
+(* type translation, pretty straightforward *)
 let rec translate_type (ty : ftype) : ftype = match ty with
   | TyBoundVar _ -> assert false
   | TyFreeVar a -> TyFreeVar a
@@ -19,6 +20,7 @@ let rec translate_type (ty : ftype) : ftype = match ty with
   | TyWhere (tl, tr, t) -> TyWhere (translate_type tl, translate_type tr, translate_type t)
 
 
+(* a type for the apply function *)
 let apply_type : ftype =
   let a1 = fresh (Identifier.mk "dummy" (1, "type")) in
   let a2 = fresh (Identifier.mk "dummy" (1, "type")) in
@@ -27,32 +29,36 @@ let apply_type : ftype =
           TyArrow (arrow_type (TyFreeVar a1) (TyFreeVar a2),
                    TyArrow (TyFreeVar a1, TyFreeVar a2))))))
 
+(* the identifier for calling the apply function *)
+let apply_id : atom = fresh (Identifier.mk "apply" (0, "term"))
+let arg_id : atom = fresh (Identifier.mk "arg"   (0, "term"))
+
+(* the apply function's body           *)
+(* take the clause list as an argument *)
 let build_apply (clauses : clause list) : fterm =
   let a1 = fresh (Identifier.mk "a1" (1, "type")) in
   let a2 = fresh (Identifier.mk "a2" (1, "type")) in
-  let apply_rec : atom = fresh (Identifier.mk "apply" (0, "term")) in
-  let apply_f   : atom = fresh (Identifier.mk "f"     (0, "term")) in
-  let apply_arg : atom = fresh (Identifier.mk "arg"   (0, "term")) in
-  TeFix (apply_rec, apply_type,
+  let f   : atom = fresh (Identifier.mk "f"     (0, "term")) in
+  TeFix (apply_id, apply_type,
          TeTyAbs (a1,
                   TeTyAbs (a2,
-                           TeAbs (apply_f,
+                           TeAbs (f,
                                   arrow_type (TyFreeVar a1) (TyFreeVar a2),
-                                  TeAbs (apply_arg,
+                                  TeAbs (arg_id,
                                          TyFreeVar a1,
-                                         TeMatch (TeVar apply_f, TyFreeVar a2, clauses),
+                                         TeMatch (TeVar f, TyFreeVar a2, clauses),
                                          ref None),
                                   ref None))))
 
-let apply_id : fterm = TeVar (fresh (Identifier.mk "apply" (0, "term")))
 
-let mk_closure x m = failwith "TODO"
-
+(* the translation itself *)
 let translate (Prog (t_table, dc_table, term) : program) =
-  let closures = Stack.create () in
+  let schemes = Stack.create () in
+  let clauses = Stack.create () in
   let rec traverse t = match t with
-    | TeVar x -> TeVar x
-    | TeAbs (x, t1, e, fun_info) ->
+    (* lambda abstraction *)
+    | TeAbs (x, _, e, fun_info) ->
+      (* get the function information from the typechecker *)
       let (hyps, tenv, t1, t2) = match !fun_info with
         | None -> failwith "You need to call the typechecker before defunctionalization"
         | Some r -> begin match r.fty with
@@ -60,17 +66,38 @@ let translate (Prog (t_table, dc_table, term) : program) =
             | _ -> assert false
             end
       in
+      (* create a fresh id for the lambda *)
       let m : atom = fresh (Identifier.mk "m" (3, "data constructor")) in
-      let () = Stack.push (mk_closure e m) closures in
-      TeData (m, failwith "TODO", failwith "TODO")
+      (* build its type scheme and store it *)
+      let free_type_vars = [] in
+      let free_vars = AtomSet.fold (fun h t -> h :: t) (fv t) [] in
+      let free_vars_types = List.map (fun a -> lookup a tenv) free_vars in
+      let sch = (foralls free_type_vars
+                   (wheres (TyArrow (TyTuple free_vars_types,
+                                     arrow_type (translate_type t1) (translate_type t2)))
+                      hyps)) in
+      let () = Stack.push (m, sch) schemes in
+      (* build its corresponding clause and store it *)
+      let c = Clause (PatData (Error.dummy, m, free_type_vars, free_vars),
+                      TeLet (x, TeVar arg_id, traverse e)) in
+      let () = Stack.push c clauses in
+      (* return the data constructor *)
+      TeData (m,
+              List.map (fun a -> TyFreeVar a) free_type_vars,
+              List.map (fun x -> TeVar x) free_vars)
+    (* function application *)
     | TeApp (e1, e2, app_info) ->
+      (* get the application information from the typechecker *)
       let (t1, t2) = match !app_info with
         | None -> failwith "You need to call the typechecker before defunctionalization"
         | Some r -> (r.domain, r.codomain)
       in
-      TeApp (TeApp (TeTyApp (TeTyApp (apply_id, t1), t2),
+      (* replace with a call to apply *)
+      TeApp (TeApp (TeTyApp (TeTyApp (TeVar apply_id, t1), t2),
                     traverse e1, ref None),
              traverse e2, ref None)
+    (* the rest is just traversal *)
+    | TeVar x -> TeVar x
     | TeLet (x, e1, e2) -> TeLet (x, traverse e1, traverse e2)
     | TeFix (x, t, e) -> TeFix (x, translate_type t, traverse e)
     | TeTyAbs (alpha, e) -> TeTyAbs (alpha, traverse e)
@@ -82,5 +109,13 @@ let translate (Prog (t_table, dc_table, term) : program) =
                List.map (fun (Clause (p, e)) -> Clause (p, traverse e)) clauses)
     | TeLoc (loc, e) -> TeLoc (loc, traverse e)
   in
-  let term' = traverse term in
-  Prog (t_table, dc_table, term')
+  (* translate the body of the program *)
+  let traversed = traverse term in
+  let t_table' = AtomMap.add arrow_tk 2 t_table in
+  let dc_table' = Stack.fold (fun res (m, sch) -> AtomMap.add m sch res) dc_table schemes in
+  let term' = TeLet (apply_id,
+                     build_apply (Stack.fold (fun res x -> x :: res) [] clauses),
+                     traversed) in
+  let res = Prog (t_table', dc_table', term') in
+  let xenv, ty = Typecheck.run res in
+  res
